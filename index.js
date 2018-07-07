97e89d27c2c7f5477104803679e8290c85ee9a48
Marc Farra <marcfarra@gmail.com>
2018-07-06 22:41:20 -0400

    Initial batching code

--- index.js
+++ index.js
@@ -1,71 +1,142 @@
-var SaxAsync = require('sax-async');
-var fs = require('fs');
+const SaxAsync = require('sax-async');
+const fs = require('fs');
+const through2 = require('through2');
+const duplexify = require('duplexify');
+
+const BATCH_SIZE = 1;
+
+
 const knex = require('knex')({
     client: 'sqlite3',
     connection: {
         filename: "./notes.sqlite"
     },
+    pool: {
+        afterCreate: (conn, cb) =>
+            conn.run(`PRAGMA synchronous = OFF`, function () {
+                conn.run('PRAGMA journal_mode = MEMORY', cb)
+            })
+    },
     useNullAsDefault: true
 });
 
-knex.schema.createTable('comments', table => {
-    table.increments('id');
-    table.integer('note_id');
-    table.float('latitude');
-    table.float('longitude');
-    table.dateTime('created_at');
-    table.dateTime('closed_at');
-    table.string('action');
-    table.datetime('timestamp');
-    table.string('uid');
-    table.string('user');
-    table.text('text');
-}).then(function () {
+function main(tr) {
     console.log('parsing');
-    parseNotes('./notes.xml', function () {
-        console.log('done');
-    })
-})
-
-function saveNote(note) {
-    var attribs = note.attributes;
-    var note_id = attribs.ID;
-    var lat = parseFloat(attribs.LAT);
-    var lon = parseFloat(attribs.LON);
-    var closed_at = attribs.CLOSED_AT;
-    var created_at = attribs.CREATED_AT;
-
-    let promises = note.comments.map((comment) => {
-        let cattribs = comment.attributes;
-        let action = cattribs.ACTION;
-        let timestamp = cattribs.TIMESTAMP;
-        let uid = cattribs.UID;
-        let text = comment.text || '';
-        return knex('comments').insert({
-            note_id,
-            latitude: lat,
-            longitude: lon,
-            closed_at: closed_at? new Date(closed_at) : null,
-            created_at: created_at? new Date(created_at) : null,
-            action,
-            timestamp: timestamp? new Date(timestamp) : null,
-            uid,
-            text
-        })
-    });
-
-    return Promise.all(promises)
+    fs.createReadStream('./notes.xml')
+        .pipe(parseNotes())
+ //       .pipe(batch)
+        .pipe(batchSave(tr))
+        .on('error', function (err) {
+            console.error(err);
+            process.exit(1);
+        });
 }
 
+
+
+/* 
+Check if table exists
+If it does, truncate the data
+else create the table
+*/
+knex.schema.hasTable('comments')
+.then(function (exists) {
+    if (!exists) {
+        console.log('creating table')
+        return knex.schema.createTable('comments', table => {
+            table.increments('id');
+            table.integer('note_id');
+            table.float('latitude');
+            table.float('longitude');
+            table.dateTime('created_at');
+            table.dateTime('closed_at');
+            table.string('action');
+            table.datetime('timestamp');
+            table.string('uid');
+            table.string('user');
+            table.text('text');
+        })
+    }
+    else {
+        console.log('table exists, truncating')
+        return knex('comments').truncate()
+    }
+})
+.then(
+    () => {
+            main(knex)
+    })
+.catch(function (err) {
+    console.error(err)
+    process.exit(1);
+})
+
+let num_comments = 0;
+const batchSave = (knex) => through2.obj(function (records, enc, callback) {
+    return knex('comments').insert(records)
+        .then(function () {
+            num_comments += BATCH_SIZE;
+            process.stdout.write(`saved ${num_comments} comments\r`)
+            return callback();
+        });
+});
+
+let buffer = []
+const batch = through2.obj(function (record, enc, callback) {
+    buffer.push(record)
+    if (buffer.length >= BATCH_SIZE) {
+        this.push(buffer)
+        delete(buffer)
+        buffer = []
+    }
+    callback()
+}, function () {
+    if (buffer.length) {
+        this.push(buffer)
+    }
+    callback()
+})
+
 /** 
- * Code mostly from
+ * Code adapted from
  * https://github.com/mapbox/osm-comments-parser/tree/master/notes
  */
-function parseNotes(xmlFilename, done) {
-    var count = 0;
-    var comments = 0;
+function parseNotes() {
     var saxStream = new SaxAsync();
     var currentNote, currentComment;
+
+    const ts = through2.obj(function (note, enc, callback) {
+        var attribs = note.attributes;
+        var note_id = attribs.ID;
+        var lat = parseFloat(attribs.LAT);
+        var lon = parseFloat(attribs.LON);
+        var closed_at = attribs.CLOSED_AT;
+        var created_at = attribs.CREATED_AT;
+
+        let comments = note.comments.map((comment) => {
+            let cattribs = comment.attributes;
+            let action = cattribs.ACTION;
+            let timestamp = cattribs.TIMESTAMP;
+            let uid = cattribs.UID;
+            let text = comment.text || '';
+            return {
+                note_id,
+                latitude: lat,
+                longitude: lon,
+                closed_at: closed_at ? new Date(closed_at) : null,
+                created_at: created_at ? new Date(created_at) : null,
+                action,
+                timestamp: timestamp ? new Date(timestamp) : null,
+                uid,
+                text
+            }
+        });
+        comments.forEach(comment => this.push(comment))
+        callback();
+    });
+
+    const dup = duplexify(saxStream, ts, {objectMode: true});
+
     saxStream.hookSync('opentag', function (node) {
         var tagName = node.name.toLowerCase();
         if (tagName === 'note') {
@@ -76,13 +147,10 @@ function parseNotes(xmlFilename, done) {
             currentComment = node;
         }
     });
-    saxStream.hookAsync('closetag', async function (next, tagName) {
+    saxStream.hookAsync('closetag', function (next, tagName) {
         tagName = tagName.toLowerCase();
         if (tagName === 'note') {
-            await saveNote(currentNote);
-            count +=1 ;
-            comments += currentNote.comments.length;
-            process.stdout.write(`saved ${count} notes, ${comments} comments\r`)
+            ts.write(currentNote);
             currentNote = null;
         } else if (tagName === 'comment') {
             currentComment = null;
@@ -95,10 +163,5 @@ function parseNotes(xmlFilename, done) {
         }
     });
 
-    saxStream.hookSync('end', function () {
-        done();
-    })
-
-    fs.createReadStream(xmlFilename)
-        .pipe(saxStream);
+    return dup;
 }
